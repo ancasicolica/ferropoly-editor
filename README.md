af4c8c81aeb789da709b169d520403a3ee7be6c5
Christian Kuster <christian.kuster@kaba.com>
2015-05-28 12:49:47 +0200

    License text added (preparing to move to GitHub)

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-The OpenShift `nodejs` cartridge documentation can be found at:
+#Ferropoly Editor
 
-http://openshift.github.io/documentation/oo_cartridge_guide.html#nodejs
+A game played on the public transport system in Switzerland. Visit www.ferropoly.ch for more details about this game.
+
+
+##Language policy
+The documentation of the project is  in English while the user interface is in German (there are no plans for the support of other
+languages).
+
+
+##Licence
+GPL V3
--- editor/app.js
+++ editor/app.js
@@ -123,6 +123,10 @@ var initServer = function () {
     console.log('%s: Node server started on %s:%d ...',
       Date(Date.now() ), app.get('ip'), app.get('port'));
   });
+  console.log('Ferropoly Editor, Copyright (C) 2015 Christian Kuster, CH-8342 Wernetshausen');
+  console.log('This program comes with ABSOLUTELY NO WARRANTY;');
+  console.log('This is free software, and you are welcome to redistribute it');
+  console.log('under certain conditions; see www.ferropoly.ch for details.');
   console.log('Ferropoly Editor server listening on port ' + app.get('port'));
 };
 
