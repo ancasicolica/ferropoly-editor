fdd735fff7b2afa66ccb792f1b6b9780d9ab95c2
Christian Kuster <christian.kuster@kaba.com>
2015-08-16 14:36:11 +0200

    preview settings added

--- editor/settings/contabo.js
+++ editor/settings/contabo.js
@@ -1,48 +1,49 @@
 /**
- * Settings for the contabo server
+ * Settings for the contabo server. Fix IP is 5.189.159.156
  * Created by kc on 15.08.15.
  */
 'use strict';
 
 
-module.exports = function(settings) {
+module.exports = function (settings) {
 
   settings.server = {
-    port: 3002,
-    host: '5.189.159.156',
-    serverId: 'editor-app.ferropoly.ch'
-  };
-
-  settings.publicServer = {
-    port: 3002,
-    host: '5.189.159.156'
-  };
-
-
-  settings.locationDbSettings = {
-    mongoDbUrl: process.env.FERROPOLY_CONNECTION_STRING
-  };
-
-  settings.cron = {
-    // [MINUTE] [HOUR] [DAY OF MONTH] [MONTH OF YEAR] [DAY OF WEEK] [YEAR (optional)]
-    createDemoGameplay : '0 1 * * *'
-  };
-
-  settings.demoGameplay = {
-    addDays : 0
-  };
-
-  settings.mailer = {
-    senderAddress: process.env.MAILER_SENDER,
-    host: process.env.MAILER_HOST,
-    port: 465,
-    secure: true,
-    auth: {
-      pass: process.env.MAILER_PASS,
-      user: process.env.MAILER_USER
-    }
-  };
-
-
-  return settings;
+    port: process.env.FERROPOLY_EDITOR_PORT,
+    host: 'app.ferropoly.ch',
+    serverId: 'editor-app.ferropoly.ch-v' + settings.version
 };
+
+settings.publicServer = {
+  port: process.env.FERROPOLY_EDITOR_PORT,
+  host: 'app.ferropoly.ch'
+};
+
+
+settings.locationDbSettings = {
+  mongoDbUrl: process.env.FERROPOLY_CONNECTION_STRING
+};
+
+settings.cron = {
+  // [MINUTE] [HOUR] [DAY OF MONTH] [MONTH OF YEAR] [DAY OF WEEK] [YEAR (optional)]
+  createDemoGameplay: '0 1 * * *'
+};
+
+settings.demoGameplay = {
+  addDays: 0
+};
+
+settings.mailer = {
+  senderAddress: process.env.MAILER_SENDER,
+  host: process.env.MAILER_HOST,
+  port: 465,
+  secure: true,
+  auth: {
+    pass: process.env.MAILER_PASS,
+    user: process.env.MAILER_USER
+  }
+};
+
+
+return settings;
+}
+;
