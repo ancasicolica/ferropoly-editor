78d41028592ad3464b5bd6354efb9a7bd10bfb7c
Christian Kuster <christian.kuster@kaba.com>
2015-08-23 20:27:03 +0200

    contabo_preview settings added

---
+++ editor/settings/contabo_preview.js
@@ -0,0 +1,51 @@
+/**
+ * Settings for the contabo server. Fix IP is 5.189.159.156
+ * Created by kc on 15.08.15.
+ */
+'use strict';
+
+
+module.exports = function (settings) {
+
+  settings.server = {
+    port: process.env.FERROPOLY_EDITOR_PORT,
+    host: 'app.ferropoly.ch',
+    serverId: 'editor-app.ferropoly.ch-v' + settings.version
+  };
+
+  settings.publicServer = {
+    port: process.env.FERROPOLY_EDITOR_PORT,
+    host: 'app.ferropoly.ch'
+  };
+
+  settings.locationDbSettings = {
+    mongoDbUrl: process.env.FERROPOLY_CONNECTION_STRING
+  };
+
+
+  settings.cron = {
+    // [MINUTE] [HOUR] [DAY OF MONTH] [MONTH OF YEAR] [DAY OF WEEK] [YEAR (optional)]
+    createDemoGameplay: '0 1 * * *'
+  };
+
+
+  settings.demoGameplay = {
+    addDays: 0
+  };
+
+  settings.mailer = {
+    senderAddress: process.env.MAILER_SENDER,
+    host: process.env.MAILER_HOST,
+    port: 465,
+    secure: true,
+    auth: {
+      pass: process.env.MAILER_PASS,
+      user: process.env.MAILER_USER
+    }
+  };
+
+  settings.mainInstances = ['http://app.ferroply.ch:3004', 'http://app.ferroply.ch:3104'];
+
+  return settings;
+}
+;
