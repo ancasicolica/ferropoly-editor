f86ff73c87db2e23d06601cd3dc27cc3915a8dd8
Christian Kuster <christian.kuster@kaba.com>
2015-08-30 15:37:35 +0200

    contabo deploy types

--- editor/settings.js
+++ editor/settings.js
@@ -3,7 +3,11 @@
  */
 var pkg = require('./../package.json'),
   fs = require('fs'),
+  _ = require('lodash');
   path = require('path');
+
+// Set default
+var deployType = process.env.DEPLOY_TYPE || 'local';
 
 var settings = {
   name: pkg.name,
@@ -13,13 +17,20 @@ var settings = {
   preview: process.env.FERROPOLY_PREVIEW // is only defined when preview is enabled
 };
 
-
-
+// Set specific deploy type
 if (process.env.OPENSHIFT_NODEJS_IP) {
-  process.env.DEPLOY_TYPE = 'openshift';
+  deployType = 'openshift';
 }
-else {
-  process.env.DEPLOY_TYPE = process.env.DEPLOY_TYPE || 'local';
+else if (process.env.DEPLOY_TYPE === 'contabo') {
+  // check which instance
+  var rootPath = path.join(__dirname, '..');
+  console.log('Root path: ' + rootPath);
+  if (_.endsWith(rootPath, 'preview')) {
+    deployType = 'contabo_preview';
+  }
+  else if (_.endsWith(rootPath, 'rc')) {
+    deployType = 'contabo_rc';
+  }
 }
 
 if (process.env.DEBUG) {
@@ -33,10 +44,10 @@ else {
   settings.minifedjs = true;
 }
 
-console.log('DEPLOY_TYPE: ' + process.env.DEPLOY_TYPE);
+console.log('DEPLOY_TYPE: ' + deployType);
 
-if (process.env.DEPLOY_TYPE && fs.existsSync(path.join(__dirname, 'settings/' + process.env.DEPLOY_TYPE + '.js'))) {
-  module.exports = require('./settings/' + process.env.DEPLOY_TYPE + '.js')(settings);
+if (deployType && fs.existsSync(path.join(__dirname, 'settings/' + deployType + '.js'))) {
+  module.exports = require('./settings/' + deployType + '.js')(settings);
 } else {
   module.exports = settings;
 }
