cabe439032b3e5b3a5b39b7c0f70c9afe56f37eb
Christian Kuster <christian@kusti.ch>
2016-01-31 17:16:02 +0100

    Added new logger functions, both including position

--- common/models/travelLogModel.js
+++ common/models/travelLogModel.js
@@ -16,11 +16,12 @@ var travelLogSchema = mongoose.Schema({
   gameId    : String,
   teamId    : String,
   propertyId: String, // EITHER propertyId
-  location  : {       // OR location coordinates must be supplied
-    lat: Number,
-    lng: Number
+  position  : {       // OR location coordinates must be supplied
+    lat     : Number,
+    lng     : Number,
+    accuracy: Number
   },
-  timestamp: {type: Date, default: Date.now}
+  timestamp : {type: Date, default: Date.now}
 });
 
 
@@ -52,6 +53,56 @@ var addEntry = function (gameId, teamId, propertyId, callback) {
   logEntry.save(callback);
 };
 
+/**
+ * Adds an entry by its property
+ * @param gameId
+ * @param teamId
+ * @param propertyId
+ * @param callback
+ * @returns {*}
+ */
+var addPropertyEntry = function (gameId, teamId, property, callback) {
+  if (!gameId || !teamId || !property) {
+    return callback(new Error('all params in addEntry must be set'));
+  }
+  if (!_.isString(gameId) || !_.isString(teamId)) {
+    return callback(new Error('teamId and gameId params in createEntry must be strings'));
+  }
+  var logEntry        = new TravelLog();
+  logEntry.gameId     = gameId;
+  logEntry.teamId     = teamId;
+  logEntry.propertyId = property.uuid;
+  logEntry.position   = {
+    lat: property.location.position.lat,
+    lng: property.location.position.lng,
+    accuracy: 200};
+  logEntry._id        = gameId + '-' + moment().format('YYMMDD-hhmmss:SSS') + '-' + _.random(100000, 999999);
+  logEntry.save(callback);
+}
+  ;
+
+/**
+ * Adds an entry by its position
+ * @param gameId
+ * @param teamId
+ * @param position
+ * @param callback
+ * @returns {*}
+ */
+var addPositionEntry = function (gameId, teamId, position, callback) {
+  if (!gameId || !teamId || !position) {
+    return callback(new Error('all params in addEntry must be set'));
+  }
+  if (!_.isString(gameId) || !_.isString(teamId)) {
+    return callback(new Error('teamId and gameId params in createEntry must be strings'));
+  }
+  var logEntry      = new TravelLog();
+  logEntry.gameId   = gameId;
+  logEntry.teamId   = teamId;
+  logEntry.position = position;
+  logEntry._id      = gameId + '-' + moment().format('YYMMDD-hhmmss:SSS') + '-' + _.random(100000, 999999);
+  logEntry.save(callback);
+};
 
 /**
  * Deletes all entries for a gameplay
@@ -111,5 +162,7 @@ module.exports = {
   addEntry        : addEntry,
   deleteAllEntries: deleteAllEntries,
   getLogEntries   : getLogEntries,
-  getAllLogEntries: getAllLogEntries
+  getAllLogEntries: getAllLogEntries,
+  addPositionEntry: addPositionEntry,
+  addPropertyEntry: addPropertyEntry
 };
