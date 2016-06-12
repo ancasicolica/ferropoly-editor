8ffa89d708a9f6f9290d8c762cb9800aeebda6ca
Christian Kuster <christian@kusti.ch>
2016-06-12 15:42:36 +0200

    Bugfix: OAuth settings for release version

--- editor/settings/contabo.js
+++ editor/settings/contabo.js
@@ -31,9 +31,9 @@ module.exports = function (settings) {
   };
 
   // Facebook settings
-  settings.oAuth.facebook.callbackURL = 'https://editor-preview.ferropoly.ch/auth/facebook/callback';
+  settings.oAuth.facebook.callbackURL = 'https://editor.ferropoly.ch/auth/facebook/callback';
   // Google Settings
-  settings.oAuth.google.callbackURL = 'https://editor-preview.ferropoly.ch/auth/google/callback';
+  settings.oAuth.google.callbackURL = 'https://editor.ferropoly.ch/auth/google/callback';
 
 
   settings.demoGameplay = {
