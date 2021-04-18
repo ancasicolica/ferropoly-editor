aa5c23ed298799b96603171d594dc7ca1b532cfb
Christian Kuster <christian@kusti.ch>
2021-04-18 13:00:09 +0200

    Preislist and Game-Selector ready to be built for prod

--- editor/webapp/webpack.prod.js
+++ editor/webapp/webpack.prod.js
@@ -1,18 +1,22 @@
-const {merge}              = require('webpack-merge');
-const common               = require('./webpack.common.js');
-const path                 = require('path');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-const HtmlWebpackPlugin    = require('html-webpack-plugin');
-const ferropolyApps        = require('./ferropolyApps.js');
+const {merge}               = require('webpack-merge');
+const common                = require('./webpack.common.js');
+const path                  = require('path');
+const BundleAnalyzerPlugin  = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const HtmlWebpackPlugin     = require('html-webpack-plugin');
+const ferropolyApps         = require('./ferropolyApps.js');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 
 // Build the webpack list
-let plugins = [new BundleAnalyzerPlugin({analyzerMode: 'static', reportFilename: 'report.html'})];
+let plugins = [
+  new BundleAnalyzerPlugin({analyzerMode: 'static', reportFilename: 'report.html'}),
+  new FaviconsWebpackPlugin(path.join(__dirname, 'favicon.png'))
+];
 ferropolyApps.forEach(app => {
   plugins.push(new HtmlWebpackPlugin(({
     chunks    : [`${app.name}`],
     template  : path.join(__dirname, '..', 'html', app.htmlFile),
     filename  : path.join(__dirname, '..', 'public', 'html', app.htmlFile),
-    publicPath: '/js/build/',
+    publicPath: '/build/',
     minify    : true
   })));
 })
@@ -23,7 +27,7 @@ module.exports = merge(common, {
   output : {
     filename     : '[name].min.js',
     chunkFilename: '[name].bundle.js',
-    path         : path.resolve(__dirname, '..', 'public', 'js', 'build')
+    path         : path.resolve(__dirname, '..', 'public', 'build')
     //path: path.resolve(__dirname, 'www', 'js')
   },
   stats  : {
@@ -35,10 +39,10 @@ module.exports = merge(common, {
     splitChunks: {
       chunks                : 'all',
       minSize               : 30000,
-      maxSize               : 250000,
+      maxSize               : 180000,
       minChunks             : 1,
-      maxAsyncRequests      : 1,
-      maxInitialRequests    : 1,
+      maxAsyncRequests      : 20,
+      maxInitialRequests    : 5,
       automaticNameDelimiter: '-',
       cacheGroups           : {
         vendors: {
