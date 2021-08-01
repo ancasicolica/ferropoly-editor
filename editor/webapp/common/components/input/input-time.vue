d35c4cf185315e09a2be6aabdce1b061ddebfc7a
Christian Kuster <christian@kusti.ch>
2021-08-01 20:20:37 +0200

    input time

--- editor/webapp/common/components/input/input-time.vue
+++ editor/webapp/common/components/input/input-time.vue
@@ -1,16 +1,15 @@
 <!---
-  A numeric input for Ferropoly
+  A time input for Ferropoly
   Christian Kuster, CH-8342 Wernetshausen, christian@kusti.ch
   Created: 01.08.21
 -->
 <template lang="pug">
-  #input-numeric
+  #input-time
     label#input-label(for="input" v-if="label") {{label}}
-    b-form-input#input(type="number"
+    b-form-timepicker#input(type="time"
       :value="value"
-      :min="min.toString()"
-      :max="max.toString()"
       :state="state"
+      locale="de"
       @input="update"
       aria-describedby="input-help input-feedback"
     )
@@ -19,27 +18,20 @@
 </template>
 
 <script>
-import InputMixin from './inputMixin.js'
+import InputMixin from './inputMixin';
+
 export default {
-  name      : 'input-numeric',
+  name      : 'input-time',
   props     : {
-    value   : {
-      type   : Number,
-      default: () => {
-        return 0;
-      }
-    },
-    min     : {
+    value: {
       type   : String,
       default: () => {
-        return '0';
+        return ('05:00:00');
       }
     },
-    max     : {
-      type   : String,
-      default: () => {
-        return '10';
-      }
+    validTime: Boolean,
+    default: () => {
+      return true;
     }
   },
   data      : function () {
@@ -50,29 +42,24 @@ export default {
   },
   computed  : {
     state() {
-      let s = (this.minimum <= this.value) && (this.value <= this.maximum);
+      let s = this.validTime;
       this.$emit('state', {id: this._uid, state: s});
       return s;
-    },
-    minimum() {
-      return parseInt(this.min);
-    },
-    maximum() {
-      return parseInt(this.max);
     }
   },
   methods   : {
     update(e) {
-      let val = parseInt(e);
-      this.$emit('input', val);
+      console.log(e);
+      this.$emit('input', e);
     }
   },
   components: {},
   filters   : {},
-  mixins: [InputMixin]
+  mixins    : [InputMixin]
 }
 </script>
 
 <style lang="scss" scoped>
 @import 'inputStyle.scss';
 </style>
+
--- editor/webapp/test/components/test-input.vue
+++ editor/webapp/test/components/test-input.vue
@@ -17,27 +17,67 @@
           max="10")
         p Wert: {{v1}}
       b-col
+    b-row
+      b-col
+        input-time(v-model="v2"
+          label="Eingabe Test Nr. 2.1"
+          help="Gib einen vernünftigen Wert ein"
+          feedback="Wert muss vor dem Ende sein sein"
+          @state="onTimeState"
+          :validTime="startTimeValid"
+          min="1",
+          max="10")
+        p Wert: {{v2}}
+      b-col
+    b-row
+      b-col
+        input-time(v-model="v3"
+          label="Eingabe Test Nr. 2.2"
+          help="Gib einen vernünftigen Wert ein"
+          feedback="Wert muss nach dem Start sein"
+          @state="onTimeState"
+          :validTime="endTimeValid"
+          min="1"
+          max="10")
+        p Wert: {{v3}}
+      b-col
 
 </template>
 
 <script>
 import InputNumeric from '../../common/components/input/input-numeric.vue'
+import InputTime from '../../common/components/input/input-time.vue'
+
 import FormValidatorMixin from '../../common/components/input/formValidatorMixin';
+import {DateTime} from 'luxon';
 
 export default {
   name      : 'test-input',
   props     : {},
   data      : function () {
     return {
-      v1: 5
+      v1: 5,
+      v2: '05:05:00',
+      v3: '20:05:00',
+      startTimeValid: true,
+      endTimeValid: true
     };
   },
   model     : {},
   created   : function () {
   },
   computed  : {},
-  methods   : {},
-  components: {InputNumeric},
+  methods   : {
+    onTimeState(s) {
+      this.onState(s);
+      let start = DateTime.fromISO(this.v2);
+      let end = DateTime.fromISO(this.v3);
+      this.startTimeValid = start < end;
+      this.endTimeValid = start < end;
+      console.log( this.startTimeValid, this.endTimeValid);
+    }
+  },
+  components: {InputNumeric, InputTime},
   filters   : {},
   mixins    : [FormValidatorMixin]
 }
