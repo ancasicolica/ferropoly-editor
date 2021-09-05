0e3ee9ce26ef2fa87cfec0ce7b345279b5222cb5
Christian Kuster <christian@kusti.ch>
2021-09-05 15:57:39 +0200

    Je 2 neue Backgrounds und Headers

--- editor/webapp/common/components/login/login.vue
+++ editor/webapp/common/components/login/login.vue
@@ -2,37 +2,37 @@
   Login control
 -->
 <template lang="pug">
-#login
-  #wrap
-    menu-bar(show-user-box=false :elements-right="menuElementsRight")
-    .login-box.ls-box-shape
-      h3 Ferropoly Editor Login
-      #login-with-password(v-if='passwordLogin')
-        form(action='login' method='post')
-          label(for='inputUserName' class='sr-only') Benutzername
-          input(type='text' name='username' id='inputUserName' class='form-control' placeholder='Benutzername' required autofocus)
-          label(for='inputPassword' class='sr-only') Passwort
-          input(type='password' name='password' id='inputPassword' class='form-control' placeholder='Passwort' required)
+  #login
+    #wrap
+      menu-bar(show-user-box=false :elements-right="menuElementsRight")
+      .login-box.ls-box-shape
+        h3 Ferropoly Editor Login
+        #login-with-password(v-if='passwordLogin')
+          form(action='login' method='post')
+            label(for='inputUserName' class='sr-only') Benutzername
+            input(type='text' name='username' id='inputUserName' class='form-control' placeholder='Benutzername' required autofocus)
+            label(for='inputPassword' class='sr-only') Passwort
+            input(type='password' name='password' id='inputPassword' class='form-control' placeholder='Passwort' required)
+            br
+            button#button-login.btn.btn-lg.btn-primary.btn-block(type='submit') Login
           br
-          button#button-login.btn.btn-lg.btn-primary.btn-block(type='submit') Login
-        br
-        button.btn.btn-xs.btn-default.btn-block(@click='enableSocialMediaLogin')
-          | Login mit Social Networks
-      #login-with-social-networks(v-if='!passwordLogin')
-        a#button-google.btn.btn-lg.btn-danger.btn-block(href='/auth/google')
-          i.fa.fa-google-plus
-          | &nbsp;Login mit Google
-        //a#button-dropbox.btn.btn-lg.btn-primary.btn-block(href='/auth/dropbox')
-          i.fa.fa-dropbox
-          | &nbsp;Login mit Dropbox
-        a#button-facebook.btn.btn-lg.btn-primary.btn-block(href='/auth/facebook')
-          i.fa.fa-facebook-official
-          | &nbsp;Login mit Facebook
-        //a#button-twitter.btn.btn-lg.btn-primary.btn-block(href='/auth/twitter')
-          i.fa.fa-twitter
-          | &nbsp;Login mit Twitter
-        button.btn.btn-sm.btn-default.btn-block(@click="enablePasswordLogin")
-          | Login mit Passwort
+          button.btn.btn-xs.btn-default.btn-block(@click='enableSocialMediaLogin')
+            | Login mit Social Networks
+        #login-with-social-networks(v-if='!passwordLogin')
+          a#button-google.btn.btn-lg.btn-danger.btn-block(href='/auth/google')
+            i.fa.fa-google-plus
+            | &nbsp;Login mit Google
+          //a#button-dropbox.btn.btn-lg.btn-primary.btn-block(href='/auth/dropbox')
+            i.fa.fa-dropbox
+            | &nbsp;Login mit Dropbox
+          a#button-facebook.btn.btn-lg.btn-primary.btn-block(href='/auth/facebook')
+            i.fa.fa-facebook-official
+            | &nbsp;Login mit Facebook
+          //a#button-twitter.btn.btn-lg.btn-primary.btn-block(href='/auth/twitter')
+            i.fa.fa-twitter
+            | &nbsp;Login mit Twitter
+          button.btn.btn-sm.btn-default.btn-block(@click="enablePasswordLogin")
+            | Login mit Passwort
 
 </template>
 
@@ -41,7 +41,7 @@ import $ from 'jquery'
 import MenuBar from '../menu-bar/menu-bar.vue'
 
 export default {
-  name      : "login",
+  name      : 'login',
   props     : {},
   data      : function () {
     return {
@@ -54,7 +54,7 @@ export default {
   model     : {},
   created   : function () {
     // Set background randomly
-    let i = (new Date().getSeconds() % 8) + 1;
+    let i = (new Date().getSeconds() % 10) + 1;
     $('body').addClass('bg' + i);
   },
   methods   : {
@@ -71,6 +71,96 @@ export default {
 </script>
 
 <style scoped>
+body {
+  /* background-image: url("/images/ferropoly_background01.jpg");*/
+  background-clip: border-box;
+  background-size: cover;
+}
+
+/* Wrapper for page content to push down footer */
+#login {
+  min-height: 100%;
+  height: auto;
+  /* Negative indent footer by its height */
+  margin: 0 auto -20px;
+  /* Pad bottom by footer height */
+  padding: 0 0 20px;
+}
+
+/* Set the fixed height of the footer here */
+#footer {
+  height: 20px;
+  background-color: black;
+  color: white;
+  z-index: 100;
+}
+
+#footer-time {
+  text-align: right;
+}
+
+.login-box {
+  max-width: 330px;
+}
+
+.signup-box {
+  max-width: 600px;
+}
+
+.agb-box {
+  max-width: 80%;
+  overflow: auto;
+
+}
+
+.agb-contents {
+  height: inherit;
+  margin-bottom: 10px;
+  padding-bottom: 20px;
+}
+
+.ls-box-shape {
+  padding: 15px;
+  margin: 0 auto;
+  background-color: rgba(255, 255, 255, 0.8);
+  border-radius: 5px;
+  margin-top: 30px;
+}
+
+.form-signin .form-signin-heading,
+.form-signin .checkbox {
+  margin-bottom: 10px;
+}
+
+.form-signin .checkbox {
+  font-weight: normal;
+}
+
+.form-signin .form-control {
+  position: relative;
+  height: auto;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+  padding: 10px;
+  font-size: 16px;
+}
+
+.form-signin .form-control:focus {
+  z-index: 2;
+}
+
+.form-signin input[type="email"] {
+  margin-bottom: -1px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+
+.form-signin input[type="password"] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
 
 
 </style>
