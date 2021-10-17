411d221b1d9af3973e01acc2cdc87c3f2c4f93b8
Christian Kuster <christian@kusti.ch>
2021-10-17 17:23:45 +0200

    typo

--- editor/routes/index.js
+++ editor/routes/index.js
@@ -7,7 +7,7 @@ const router   = express.Router();
 const path     = require('path');
 
 /**
- * Send Pricelist HTML Page
+ * Send HTML Page
  */
 router.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '..', 'public', 'html', 'game-selector.html'));
