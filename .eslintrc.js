52314f4ba7e6b54546f54b09d495b389436800e7
Christian Kuster <christian@kusti.ch>
2022-02-24 21:15:53 +0100

    eslint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,14 @@
  */
 
 module.exports = {
+  env: {
+    node: true,
+    es6: true,
+  },
   extends: [
     // add more generic rulesets here, such as:
-     //'eslint:recommended',
-     'plugin:vue/recommended' // Use this if you are using Vue.js 2.x.
+    //'eslint:recommended',
+    'plugin:vue/recommended' // Use this if you are using Vue.js 2.x.
   ],
   rules: {
     // override/add rules settings here, such as:
