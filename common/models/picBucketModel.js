681c2b5d2a618d30691e333b9923868dc76d8410
Christian Kuster <christian@kusti.ch>
2023-03-06 20:45:49 +0100

    picBucketModel.js update

--- common/models/picBucketModel.js
+++ common/models/picBucketModel.js
@@ -9,21 +9,23 @@ const logger          = require('../lib/logger').getLogger('picBucketModel');
  * The mongoose schema for a picture
  */
 const picBucketSchema = mongoose.Schema({
-  _id       : String,
-  gameId    : String,
-  teamId    : String, // Set only if relevant, otherwise undefined
-  filename  : String,
-  message   : String, // This is a message for the picture
-  url       : String, // The public URL
-  user      : String,
-  propertyId: String, // Property ID of an associated property (if any)
-  position  : {
+  _id             : String,
+  gameId          : String,
+  teamId          : String, // Set only if relevant, otherwise undefined
+  filename        : String,
+  message         : String, // This is a message for the picture
+  url             : String, // The public URL
+  thumbnail       : String, // URL to the thumbnail
+  user            : String,
+  propertyId      : String, // Property ID of an associated property (if any)
+  position        : {
     lat     : Number,
     lng     : Number,
     accuracy: Number
   },
-  uploaded  : {type: Boolean, default: false},
-  timestamp : {type: Date, default: Date.now}
+  uploaded        : {type: Boolean, default: false},
+  timestamp       : {type: Date, default: Date.now},
+  lastModifiedDate: Date
 });
 
 
