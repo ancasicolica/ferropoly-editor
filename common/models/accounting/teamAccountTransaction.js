ff594b5756e18e558bbecc70aa4d6706be5b18b0
Christian Kuster <christian@kusti.ch>
2023-04-09 09:23:13 +0200

    WIP: Large refactoring with models (new mongoose version)
    teamAccountTransaction.js

--- common/models/accounting/teamAccountTransaction.js
+++ common/models/accounting/teamAccountTransaction.js
@@ -4,19 +4,18 @@
  * Created by kc on 19.04.15.
  */
 
-
-var mongoose                     = require('mongoose');
-var moment                       = require('moment');
-var logger                       = require('../../lib/logger').getLogger('teamAccountTransaction');
-var _                            = require('lodash');
+const mongoose                     = require('mongoose');
+const moment                       = require('moment');
+const logger                       = require('../../lib/logger').getLogger('teamAccountTransaction');
+const _                            = require('lodash');
 /**
  * The mongoose schema for a team account
  */
-var teamAccountTransactionSchema = mongoose.Schema({
+const teamAccountTransactionSchema = mongoose.Schema({
   gameId   : String, // Game the transaction belongs to
   timestamp: {type: Date, default: Date.now}, // Timestamp of the transaction
   teamId   : String, // This is the uuid of the team the account belongs to
-  user     : String, // The user (one of the admins) which was initiating the transaction
+  user     : {type: String, default: 'admin'}, // The user (one of the admins) which was initiating the transaction
 
   transaction: {
     /*
@@ -31,10 +30,10 @@ var teamAccountTransactionSchema = mongoose.Schema({
     info  : String, // Info about the transaction
     /*
      If the transaction consists of several other transactions (interests every hour), we do not create
-     a specific TeamAccountTransaction for every property. Instead there is an array having the same
+     a specific TeamAccountTransaction for every property. Instead, there is an array having the same
      data as this transaction (except this array)
      */
-    parts : Array
+    parts: Array
   }
 
 }, {autoIndex: true});
@@ -42,21 +41,26 @@ var teamAccountTransactionSchema = mongoose.Schema({
 /**
  * The Gameplay model
  */
-var TeamAccountTransaction = mongoose.model('TeamAccountTransactions', teamAccountTransactionSchema);
+const TeamAccountTransaction = mongoose.model('TeamAccountTransactions', teamAccountTransactionSchema);
 
 /**
  * Book the transaction
  * @param transaction
  * @param callback
  */
-function book(transaction, callback) {
-  if (transaction.transaction.parts) {
-    transaction.transaction.parts = _.sortBy(transaction.transaction.parts, 'propertyName');
+async function book(transaction, callback) {
+  try {
+    if (transaction.transaction.parts) {
+      transaction.transaction.parts = _.sortBy(transaction.transaction.parts, 'propertyName');
+    }
+    const res = await transaction.save();
+    callback(null, res);
+  } catch (ex) {
+    logger.error(ex);
+    callback(ex);
   }
-  transaction.save(function (err) {
-    callback(err);
-  });
 }
+
 
 /**
  * Book two transactions: the debitor and creditor bookings
@@ -64,16 +68,18 @@ function book(transaction, callback) {
  * @param creditor
  * @param callback
  */
-function bookTransfer(debitor, creditor, callback) {
-  debitor.save(function (err) {
-    if (err) {
-      callback(err);
-    }
-    creditor.save(function (err) {
-      callback(err);
-    });
-  });
+async function bookTransfer(debitor, creditor, callback) {
+  try {
+    await debitor.save();
+    await creditor.save();
+    callback(null);
+  } catch (ex) {
+    logger.error(ex);
+    callback(ex);
+  }
+
 }
+
 
 /***
  * Get the entries of the account
@@ -84,70 +90,93 @@ function bookTransfer(debitor, creditor, callback) {
  * @param callback
  * @returns {*}
  */
-function getEntries(gameId, teamId, tsStart, tsEnd, callback) {
-  if (!gameId) {
-    return callback(new Error('parameter error, missing gameId'));
-  }
+async function getEntries(gameId, teamId, tsStart, tsEnd, callback) {
+  try {
+    if (!gameId) {
+      return callback(new Error('parameter error, missing gameId'));
+    }
+    if (!tsStart) {
+      tsStart = moment('2015-01-01');
+    }
+    if (!tsEnd) {
+      tsEnd = moment();
+    }
 
-  if (!tsStart) {
-    tsStart = moment('2015-01-01');
-  }
-  if (!tsEnd) {
-    tsEnd = moment();
-  }
-  if (teamId) {
-    // Only of one team
-    TeamAccountTransaction.find({gameId: gameId})
-      .where('teamId').equals(teamId)
-      .where('timestamp').gt(tsStart.toDate()).lte(tsEnd.toDate())
-      .sort('timestamp')
-      .lean()
-      .exec(function (err, data) {
-        callback(err, data);
-      })
-  } else {
-    // all teams
-    TeamAccountTransaction.find({gameId: gameId})
-      .where('timestamp').gt(tsStart.toDate()).lte(tsEnd.toDate())
-      .sort('timestamp')
-      .lean()
-      .exec(function (err, data) {
-        callback(err, data);
-      })
+    if (teamId) {
+      // Only of one team
+      const data = await TeamAccountTransaction
+        .find({gameId: gameId})
+        .where('teamId').equals(teamId)
+        .where('timestamp').gt(tsStart.toDate()).lte(tsEnd.toDate())
+        .sort('timestamp')
+        .lean()
+        .exec();
+      return callback(null, data);
+    } else {
+      // all teams
+      const data = await TeamAccountTransaction
+        .find({gameId: gameId})
+        .where('timestamp').gt(tsStart.toDate()).lte(tsEnd.toDate())
+        .sort('timestamp')
+        .lean()
+        .exec()
+      return callback(null, data);
+    }
+  } catch (ex) {
+    logger.error(ex);
+    callback(ex);
   }
 }
+
 
 /**
  * Dumps all data for a gameplay (when deleting the game data)
  * @param gameId
  * @param callback
  */
-function dumpAccounts(gameId, callback) {
-  if (!gameId) {
-    return callback(new Error('No gameId supplied'));
+async function dumpAccounts(gameId, callback) {
+  try {
+    if (!gameId) {
+      return callback(new Error('No gameId supplied'));
+    }
+    logger.info('Removing all account information for ' + gameId);
+    const res = await TeamAccountTransaction
+      .deleteMany({gameId: gameId})
+      .exec();
+    callback(null, res);
+  } catch (ex) {
+    logger.error(ex);
+    callback(ex);
   }
-  logger.info('Removing all account information for ' + gameId);
-  TeamAccountTransaction.deleteMany({gameId: gameId}, callback)
 }
+
 
 /**
  * Returns the sum of all accounts of a game
  * @param gameId
  * @param callback
  */
-function getRankingList(gameId, callback) {
-  TeamAccountTransaction.aggregate([
-    {
-      $match: {
-        gameId: gameId,
-      }
-    }, {
-      $group: {
-        _id  : '$teamId',
-        asset: {$sum: "$transaction.amount"}
-      }
-    }
-  ], callback);
+async function getRankingList(gameId, callback) {
+  try {
+    const res = await TeamAccountTransaction
+      .aggregate([
+        {
+          $match: {
+            gameId: gameId,
+          }
+        }, {
+          $group: {
+            _id  : '$teamId',
+            asset: {$sum: "$transaction.amount"}
+          }
+        }
+      ])
+      .exec();
+    callback(null, res);
+  } catch (ex) {
+    logger.error(ex);
+    callback(ex);
+  }
 }
 
 
@@ -156,42 +185,45 @@ function getRankingList(gameId, callback) {
  * @param gameId
  * @param callback
  */
-function getBalance(gameId, teamId, callback) {
+async function getBalance(gameId, teamId, callback) {
+  try {
+    let retVal = {};
+    const data = await TeamAccountTransaction
+      .aggregate([
+        {
+          $match: {
+            gameId: gameId,
+            teamId: teamId
+          }
+        }, {
+          $group: {
+            _id  : 'balance',
+            asset: {$sum: "$transaction.amount"}
+          }
+        }
+      ])
+      .exec();
 
-  var retVal = {};
-
-  TeamAccountTransaction.aggregate([
-    {
-      $match: {
-        gameId: gameId,
-        teamId: teamId
-      }
-    }, {
-      $group: {
-        _id  : 'balance',
-        asset: {$sum: "$transaction.amount"}
-      }
-    }
-  ], function (err, data) {
-    if (err) {
-      return callback(err);
-    }
     if (!data || data.length === 0) {
       return callback(null, {asset: 0, count: 0});
     }
     retVal.asset = data[0].asset;
 
-    TeamAccountTransaction.countDocuments({
-      gameId: gameId,
-      teamId: teamId
-    }, function (err, result) {
-      if (err) {
-        return callback(err);
-      }
-      retVal.count = result;
-      callback(null, retVal);
-    });
-  });
+    const result = await TeamAccountTransaction
+      .countDocuments({
+        gameId: gameId,
+        teamId: teamId
+      })
+      .exec();
+
+    retVal.count = result;
+    return callback(null, retVal);
+
+  } catch (ex) {
+    logger.error(ex);
+    callback(ex);
+  }
+
 }
 
 
---
+++ test/unit/common/models/teamAccountTransactionTests.js
@@ -0,0 +1,190 @@
+/**
+ * Testing team account Transactions
+ * Christian Kuster, CH-8342 Wernetshausen, christian@kusti.ch
+ * Created: 09.04.23
+ **/
+
+const expect   = require('expect.js');
+const db       = require('./../../../../common/lib/ferropolyDb');
+const tat      = require('./../../../../common/models/accounting/teamAccountTransaction');
+const settings = require('./../../../../editor/settings');
+
+const gameId     = 'unit-test';
+const propertyId = 'prop-id-test';
+const teamIds    = ['team-id-1', 'team-id-2'];
+describe('Chancellery Transaction Tests', function () {
+  before(function (done) {
+    db.init(settings, function () {
+      tat.dumpAccounts(gameId, done)
+    });
+  });
+
+  // Close DB afterwards
+  after(function (done) {
+    tat.dumpAccounts(gameId, () => {
+      db.close(done);
+    });
+  });
+
+  describe('Booking some transactions for team 1', () => {
+    it('should book a new transaction 1', done => {
+      const entry                       = new tat.Model();
+      entry.gameId                      = gameId;
+      entry.teamId                      = teamIds[0];
+      entry.transaction.origin.uuid     = teamIds[1];
+      entry.transaction.origin.category = 'team';
+      entry.transaction.amount          = 1000;
+      entry.transaction.info            = 'hello';
+      tat.book(entry, (err, res) => {
+        console.log(res);
+        expect(res.gameId).to.be(gameId);
+        done(err);
+      })
+    });
+    it('should book a new transaction 2', done => {
+      const entry                       = new tat.Model();
+      entry.gameId                      = gameId;
+      entry.teamId                      = teamIds[0];
+      entry.transaction.origin.uuid     = teamIds[1];
+      entry.transaction.origin.category = 'team';
+      entry.transaction.amount          = -500;
+      entry.transaction.info            = 'hello';
+      tat.book(entry, (err, res) => {
+        console.log(res);
+        expect(res.gameId).to.be(gameId);
+        done(err);
+      })
+    });
+    it('should book a new transaction 2', done => {
+      const entry                       = new tat.Model();
+      entry.gameId                      = gameId;
+      entry.teamId                      = teamIds[0];
+      entry.transaction.origin.uuid     = teamIds[1];
+      entry.transaction.origin.category = 'team';
+      entry.transaction.amount          = 3300;
+      entry.transaction.info            = 'hello';
+      tat.book(entry, (err, res) => {
+        console.log(res);
+        expect(res.gameId).to.be(gameId);
+        done(err);
+      })
+    });
+
+
+  });
+
+  describe('Booking some transactions for team 2', () => {
+    it('should book a new transaction 1', done => {
+      const entry                       = new tat.Model();
+      entry.gameId                      = gameId;
+      entry.teamId                      = teamIds[1];
+      entry.transaction.origin.uuid     = teamIds[0];
+      entry.transaction.origin.category = 'team';
+      entry.transaction.amount          = 500;
+      entry.transaction.info            = 'hello';
+      tat.book(entry, (err, res) => {
+        console.log(res);
+        expect(res.gameId).to.be(gameId);
+        done(err);
+      })
+    });
+    it('should book a new transaction 2', done => {
+      const entry                       = new tat.Model();
+      entry.gameId                      = gameId;
+      entry.teamId                      = teamIds[1];
+      entry.transaction.origin.uuid     = teamIds[0];
+      entry.transaction.origin.category = 'team';
+      entry.transaction.amount          = 1500;
+      entry.transaction.info            = 'hello';
+      tat.book(entry, (err, res) => {
+        console.log(res);
+        expect(res.gameId).to.be(gameId);
+        done(err);
+      })
+    });
+    it('should book a new transaction 2', done => {
+      const entry                       = new tat.Model();
+      entry.gameId                      = gameId;
+      entry.teamId                      = teamIds[1];
+      entry.transaction.origin.uuid     = teamIds[0];
+      entry.transaction.origin.category = 'team';
+      entry.transaction.amount          = 7000;
+      entry.transaction.info            = 'hello';
+      tat.book(entry, (err, res) => {
+        console.log(res);
+        expect(res.gameId).to.be(gameId);
+        done(err);
+      })
+    });
+
+
+  });
+
+  describe('Booking a transfer', () => {
+    it('should execute the transfer', done => {
+      const debitor                        = new tat.Model();
+      debitor.gameId                       = gameId;
+      debitor.teamId                       = teamIds[1];
+      debitor.transaction.origin.uuid      = teamIds[0];
+      debitor.transaction.origin.category  = 'team';
+      debitor.transaction.amount           = -250;
+      debitor.transaction.info             = 'hello';
+      const creditor                       = new tat.Model();
+      creditor.gameId                      = gameId;
+      creditor.teamId                      = teamIds[0];
+      creditor.transaction.origin.uuid     = teamIds[1];
+      creditor.transaction.origin.category = 'team';
+      creditor.transaction.amount          = 250;
+      creditor.transaction.info            = 'hello';
+
+      tat.bookTransfer(debitor, creditor, err => {
+        done(err);
+      })
+    })
+  })
+
+  describe('Getting entries', () => {
+    it('should return the entries for a single team', done => {
+      tat.getEntries(gameId, teamIds[0], undefined, undefined, (err, data) => {
+        console.log(data);
+        expect(data.length).to.be(4);
+        expect(data[1].teamId).to.be(teamIds[0]);
+        expect(data[2].teamId).to.be(teamIds[0]);
+        done(err);
+      })
+    })
+    it('should return the entries for all teams', done => {
+      tat.getEntries(gameId, undefined, undefined, undefined, (err, data) => {
+        console.log(data);
+        expect(data.length).to.be(8);
+        done(err);
+      })
+    })
+  })
+
+  describe('Getting ranking list', () => {
+    it('should return the list', done => {
+      tat.getRankingList(gameId, (err, list) => {
+        console.log(list);
+        expect(list.length).to.be(2);
+        expect(list[0]._id).to.be(teamIds[0]);
+        expect(list[0].asset).to.be(4050);
+        expect(list[1]._id).to.be(teamIds[1]);
+        expect(list[1].asset).to.be(8750);
+        done(err);
+      })
+    })
+  });
+
+  describe('Getting balance', () => {
+    it('returns the balance for a specific team', done => {
+      tat.getBalance(gameId, teamIds[0], (err, res) => {
+        console.log(res);
+        expect(res.asset).to.be(4050);
+        expect(res.count).to.be(4);
+        done(err);
+      })
+    })
+  })
+});
+
