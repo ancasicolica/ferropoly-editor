2d18a840c7ec478a226c4d2088068e4177e7edba
Christian Kuster <christian@kusti.ch>
2023-10-22 10:57:22 +0200

    Added optional "hidden" event, which was an issue in the game when buying locations

--- editor/webapp/common/components/modal-info-yes-no/modal-info-yes-no.vue
+++ editor/webapp/common/components/modal-info-yes-no/modal-info-yes-no.vue
@@ -4,19 +4,20 @@
   29.4.2021 KC
 -->
 <template lang="pug">
-  div
-    b-modal(ref="modal-info"
-      :title="title"
-      :size="size"
-      header-bg-variant="info"
-      hide-header-close=true
-      cancel-title="Nein"
-      ok-title="Ja"
-      @cancel="deny",
-      @ok="confirm")
-      .modal-body
-        div(v-html="info")
-        div(v-html="message")
+div
+  b-modal(ref="modal-info"
+    :title="title"
+    :size="size"
+    header-bg-variant="info"
+    hide-header-close=true
+    cancel-title="Nein"
+    ok-title="Ja"
+    @cancel="deny",
+    @ok="confirm",
+    @hidden="onHidden")
+    .modal-body
+      div(v-html="info")
+      div(v-html="message")
 </template>
 
 <script>
@@ -73,6 +74,9 @@ export default {
       if (this.callback) {
         this.callback(true, this.context);
       }
+    },
+    onHidden() {
+      this.$emit('hidden');
     }
   }
 }
