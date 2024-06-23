0475a6b4a2ea3cdce9eae465dc0febb0568ac810
Christian Kuster <christian@kusti.ch>
2024-06-23 18:17:20 +0200

    WIP: Editor with input field tests

--- webapp-v4/apps/test/components/FormInputTest.vue
+++ webapp-v4/apps/test/components/FormInputTest.vue
@@ -6,8 +6,12 @@
 <template lang="pug">
   .m-4
     h1 Input Test
-    input-text(v-model = "text1")
+    div
+      input-text.test(ref="test1" v-model = "text1" min-length="3" max-length="5" )
+    div
+      input-text.test2(v-model = "text2" max-length="30")
     p Text: "{{text1}}"
+
 
 </template>
 <script>
@@ -18,12 +22,13 @@ export default {
   name      : 'FormInputTest',
   components: {InputText},
   filters   : {},
-  mixins    : [],
+
   model     : {},
   props     : {},
   data      : function () {
     return {
-      text1 : 'hello'
+      text1 : '12345678901234567890',
+      text2 : '12345678901234567890'
     }
   },
   computed  : {},
@@ -36,5 +41,11 @@ export default {
 
 
 <style scoped lang="scss">
-
+.test {
+  background-color: darkseagreen;
+  width:40%
+}
+.test2 {
+  width:60%
+}
 </style>
--- webapp-v4/common/components/InputText.vue
+++ webapp-v4/common/components/InputText.vue
@@ -4,13 +4,18 @@
   Created: 23.06.2024
 -->
 <template lang="pug">
-  div
-    prime-input-text(type="text" v-model="value" @update:modelValue="emitValue")
+  prime-input-text(
+    type="text"
+    v-model="value"
+    :maxlength="maxLength"
+    :invalid="invalid"
+  )
 
 </template>
 <script>
 
 import PrimeInputText from 'primevue/inputtext';
+
 
 export default {
   name      : 'InputText',
@@ -22,27 +27,49 @@ export default {
       type   : String,
       default: ''
     },
+    minLength : {
+      type   : String,
+      default: "0"
+    },
+    maxLength : {
+      type   : String,
+      default: "60"
+    }
   },
   data      : function () {
-    return {}
+    return {
+      errorText: undefined
+    }
   },
-  computed  : {
+
+  computed: {
     value: {
       get() {
         return this.modelValue;
       },
       set(value) {
+        if (value && value.length > this.maxLength) {
+          value = value.substring(0, this.maxLength);
+        }
         this.$emit('update:modelValue', value);
       }
+    },
+    invalid() {
+      if (this.modelValue && this.modelValue.length < parseInt(this.minLength)) {
+        this.errorText = `Der Text muss mindestens ${this.minLength} Zeichen lang sein.`;
+        return true;
+      }
+      if (this.modelValue && this.modelValue.length > this.maxLength) {
+        this.errorText = `Der Text darf maximal ${this.maxLength} Zeichen lang sein.`;
+        return true;
+      }
+      this.errorText = null;
+      return false;
     }
   },
-  created   : function () {
-  },
-  methods   : {
-    emitValue(event) {
-      this.value = event;
-    }
+  created : function () {
   }
+
 }
 
 </script>
