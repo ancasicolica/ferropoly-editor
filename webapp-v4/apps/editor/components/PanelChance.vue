74cddae6186aeaabc7a59c6efe99f91e5a49d6d7
Christian Kuster <christian@kusti.ch>
2024-12-30 17:00:40 +0100

    WIP #66 feat(editor): add save button and error handling to PanelChance
    - Add "Save and Continue" button with primary styling
    - Implement error message display for API failures
    - Integrate saveGameplay method from GameplayStore

--- webapp-v4/apps/editor/components/PanelChance.vue
+++ webapp-v4/apps/editor/components/PanelChance.vue
@@ -9,27 +9,52 @@
       chance-settings
     .col-6
       gambling-settings
+      prime-button.mt-3(severity="primary" @click="onSave") Speichern und weiter
+      .div.mt-2(v-if="apiErrorMessage")
+        prime-message(severity="error") {{apiErrorMessage}}
 
 </template>
 <script>
 
 import ChanceSettings from './chance/ChanceSettings.vue';
 import GamblingSettings from './chance/GamblingSettings.vue';
+import PrimeButton from 'primevue/button';
+import {useGameplayStore} from '../../../lib/store/GamePlayStore';
+import PrimeMessage from 'primevue/message';
 
 export default {
-  name: "PanelChance",
-  components: {GamblingSettings, ChanceSettings},
-  filters   : {},
-  mixins    : [],
-  model     : {},
-  props     : {},
-  data      : function () {
-    return {}
+  name:       'PanelChance',
+  components: {GamblingSettings, ChanceSettings, PrimeButton, PrimeMessage},
+  filters:    {},
+  mixins:     [],
+  model:      {},
+  props:      {},
+  data:       function () {
+    return {
+      errorMessage: undefined
+    }
   },
-  computed  : { },
-  created   : function () {
+  computed:   {
+
+    apiErrorMessage() {
+      return this.errorMessage;
+    }
   },
-  methods   : {}
+  created:    function () {
+  },
+  methods:    {
+    async onSave() {
+      console.log('save');
+      const result = await useGameplayStore().saveGameplay();
+      if (!result?.success) {
+        // ERROR
+        console.log('no good', result)
+        this.errorMessage = result?.message;
+        return;
+      }
+      this.$router.push('/properties');
+    }
+  }
 }
 
 </script>
