6824dfe304113169c0327e61f672099fb1d346c0
Christian Kuster <christian@kusti.ch>
2025-01-17 20:51:35 +0100

    feat(editor): enhance PropertySelected component
    - Add accessibility and price range selection
    - Integrate primevue SelectButton and formatAccessibility
    - Use Pinia store bindings for selectedProperty
    - Improve UI with bold title styling

--- webapp-v4/apps/editor/components/properties/PropertySelected.vue
+++ webapp-v4/apps/editor/components/properties/PropertySelected.vue
@@ -5,38 +5,80 @@
 -->
 <template lang="pug">
   ferro-card(title="Ausgewähltes Ort" condensed)
-    div blablabla
+    div.title {{name}}
+    div(v-if="propertySelected")
+      div Erreichbarkeit: {{formatAccessibility(accessiblity)}}
+      div
+        select-button.mt-2(v-model="selectedProperty.pricelist.priceRange"
+          :options="priceRanges"
+          optionLabel="name"
+          optionValue="value"
+          size="small"
+          @value-change="onPriceRangeChanged")
+    div(v-if="!propertySelected")
+      div &nbsp;
+      div &nbsp;
 </template>
-<script>
+<script setup>
 
 import FerroCard from '../../../../common/components/FerroCard.vue'
+import SelectButton from 'primevue/selectbutton';
+import {formatAccessibility} from '../../../../common/lib/formatters';
 
-export default {
-  name:  "PropertySelected",
-  components: {FerroCard},
-  filters   : {},
-  mixins    : [],
-  model     : {},
-  props     : {
-    property: {
-      type: Object,
-      default: () => {
-        return null;
-      }
+import {get} from 'lodash';
+
+import {storeToRefs} from 'pinia';
+import {useEditorPropertiesStore} from '../../../../lib/store/EditorPropertiesStore';
+
+import {computed, defineProps} from 'vue'
+
+const props = defineProps({
+  property: {
+    type:    Object,
+    default: () => {
+      return null;
     }
-  },
-  data      : function () {
-    return {}
-  },
-  computed  : { },
-  created   : function () {
-  },
-  methods   : {}
+  }
+})
+
+const priceRanges = [
+  {name: 'unbenutzt', value: -1},
+  {name: 'sehr billig', value: 0},
+  {name: 'billig', value: 1},
+  {name: 'unteres Mittelfeld', value: 2},
+  {name: 'oberes Mittelfeld', value: 3},
+  {name: 'teuer', value: 4},
+  {name: 'sehr teuer', value: 5}
+]
+
+const name = computed(
+    () => {
+      return get(props.property, 'location.name', 'Bitte Ort auswählen');
+    }
+);
+
+const propertySelected = computed(() => {
+  return props.property !== null;
+});
+
+const accessiblity = computed(() => {
+  return get(props.property, 'location.accessibility', 'Bitte Ort auswählen');
+});
+
+
+function onPriceRangeChanged(info) {
+  console.log('PR changed', info);
 }
+
+const editorPropertiesStore = useEditorPropertiesStore();
+const {selectedProperty}    = storeToRefs(editorPropertiesStore);
+
 
 </script>
 
 
 <style scoped lang="scss">
-
+.title {
+  font-weight: bold;
+}
 </style>
