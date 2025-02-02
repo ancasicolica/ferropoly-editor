96f918aceea870f9e5f8e3f16c132c26142e50f9
Christian Kuster <christian@kusti.ch>
2025-02-02 11:33:07 +0100

    Registration #67 feat(registration): add RegistrationDeadlineStatus component for deadline notifications

---
+++ webapp-v4/apps/registration/components/RegistrationDeadlineStatus.vue
@@ -0,0 +1,34 @@
+<!---
+  Info message about deadline
+  Christian Kuster, CH-8342 Wernetshausen, christian@kusti.ch
+  Created: 28.01.2025
+-->
+
+<template>
+  <div class="mt-3">
+    <message severity="error" v-if="registrationFinished">Die Anmeldefrist ist abgelaufen, Online-Anmeldungen sind nicht
+      mehr möglich.
+    </message>
+    <message severity="warn" v-if="registrationEndingSoon">Die Anmeldefrist endet bald!</message>
+    <message severity="success" v-if="registrationActive">Die Online-Anmeldung läuft, Anmeldungen können online
+      eingereicht werden.
+    </message>
+  </div>
+
+</template>
+
+<script setup>
+
+import Message from 'primevue/message';
+import {useGameplayStore} from '../../../lib/store/GamePlayStore';
+import {storeToRefs} from 'pinia';
+
+const gamePlayStore = useGameplayStore();
+
+const {registrationFinished, registrationActive, registrationEndingSoon} = storeToRefs(gamePlayStore);
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
