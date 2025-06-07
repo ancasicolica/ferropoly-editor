292b5356bff12d360082386713e2cd9346574a76
Christian Kuster <christian@kusti.ch>
2025-06-07 11:02:04 +0200

    feat(pricelist): add finalization restrictions and error message
    - Add computed properties to manage finalization logic
    - Display error message if finalizing is no longer possible

--- webapp-v4/apps/pricelist/components/PricelistSummary.vue
+++ webapp-v4/apps/pricelist/components/PricelistSummary.vue
@@ -14,8 +14,11 @@
       <div class="flex ml-2 mr-2 mb-8">
         <pricelist-info></pricelist-info>
       </div>
-      <div class="flex ml-2 mr-2" v-if="!finalized">
+      <div class="flex ml-2 mr-2" v-if="finalizePossible">
         <pricelist-finalize></pricelist-finalize>
+      </div>
+      <div class="flex ml-2 mr-2" v-if="missedFinalizingTimeslot">
+        <Message severity="error">Der Spielstart ist bereits vorbei, deshalb kann das Spiel nicht mehr finalisiert werden. Wenn Du die Spieldaten weiter verwenden willst, dann exportiere das Spiel und erstelle ein neues Spiel mit einem Spieldatum in der Zukunft.</Message>
       </div>
     </div>
   </div>
@@ -28,11 +31,24 @@ import PricelistInfo from './PricelistInfo.vue';
 import PricelistFinalize from './PricelistFinalize.vue';
 import {useGameplayStore} from '../../../lib/store/GamePlayStore';
 import {storeToRefs} from 'pinia';
+import {computed} from 'vue';
+import {DateTime} from 'luxon';
+
+import Message from 'primevue/message';
 
 const gameplayStore  = useGameplayStore();
-const {gamename, finalized} = storeToRefs(gameplayStore);
+const {gamename, finalized, gameStart} = storeToRefs(gameplayStore);
 
+const gameAlreadyOver = computed(()=> {
+  return gameStart.value < DateTime.now();
+})
+const finalizePossible = computed(()=> {
+  return !finalized.value && !gameAlreadyOver.value;
+})
 
+const missedFinalizingTimeslot = computed(()=> {
+  return !finalized.value && gameAlreadyOver.value;
+})
 
 </script>
 
