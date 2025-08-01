e9602607ac11da0a8ca64bd386d5b202c0f02f2e
Christian Kuster <christian@kusti.ch>
2025-08-01 17:00:47 +0200

    **refactor(components, styles): convert Pug to HTML and enhance layout**
    - Convert `AboutRoot.vue` from Pug to HTML for improved maintainability.
    - Refactor component structure and layout for better readability and styling consistency.
    - Add and update Tailwind utility classes in `style.css` for enhanced visual design.

--- webapp-v4/common/components/AboutRoot.vue
+++ webapp-v4/common/components/AboutRoot.vue
@@ -3,50 +3,82 @@
   Christian Kuster, CH-8342 Wernetshausen, christian@kusti.ch
   Created: 09.06.2024
 -->
-<template lang="pug">
-  menu-bar(:elements="menuBarElements" show-user-box)
-  .grid.m-1
-    div(class="lg:col-4 md:col-6 sm:col-12")
-      FerroCard(title="Hilfe & Infos")
-        div Hilfe zur Software, Spielregeln und weitere Infos zum Spiel sind auf der Ferropoly Webseite zu finden:&nbsp;
-          a(href="https://www.ferropoly.ch" target="_blank") www.ferropoly.ch
+<template>
+  <div>
+    <menu-bar :elements="menuBarElements" show-user-box></menu-bar>
+    <div class="ferropoly-container md:grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
+      <div class="ml-1 mr-1">
+        <ferro-card title="Hilfe & Infos">
+          <div>Hilfe zur Software, Spielregeln und weitere Infos zum Spiel sind auf der Ferropoly Webseite zu finden:
+            <a href="https://www.ferropoly.ch" target="_blank">www.ferropoly.ch</a></div>
+        </ferro-card>
 
-      FerroCard.mt-1(title="Sponsoring")
-        div Diese Software darf gratis für die Durchführung von Spielen verwendet werden. Hinter dieser Software steckt viel Arbeit und für den Betrieb der Server wende ich einen Teil meines Sackgeldes wie auch meiner Freizeit auf.
-        div Deshalb würde ich mich darüber sehr freuen, wenn Dir ein ausgetragenes Spiel einen Beitrag wert wäre!
-        prime-button.mt-2(label="Beitrag bezahlen" @click="ezActive=true")
+        <ferro-card title="Sponsoring" class="mt-2">
+          <div>Diese Software darf gratis für die Durchführung von Spielen verwendet werden. Hinter dieser Software
+            steckt viel
+            Arbeit und für den Betrieb der Server wende ich einen Teil meines Sackgeldes wie auch meiner Freizeit auf.
+            <p>Deshalb würde ich mich darüber sehr freuen, wenn Dir ein ausgetragenes Spiel einen Beitrag wert wäre!</p>
+          </div>
+          <prime-button label="Beitrag bezahlen" @click="ezActive=true" class="mt-4"></prime-button>
+        </ferro-card>
+      </div>
+      <div  class="ml-1 mr-1">
+        <ferro-card title="Datenschutz">
+          <div> Es werden nur für das Spiel notwendige Daten erfasst. Mit dem Login über Google und Microsoft sind die
+            öffentlich
+            zugänglichen Daten:
+            <ul>
+              <li>Name</li>
+              <li>Email-Adresse</li>
+              <li>Avatar Bild</li>
+            </ul>
+            <p>Während dem Spiel werden zudem die Positionsdaten der teilnehmenden Personen aufgenommen. Sämtliche
+              während
+              dem Spiel erfassten
+              Daten werden 30 Tage nach dem Spiel automatisch gelöscht.</p>
+            <p>Die vollständigen Datenschutz-Infos sind auf der Seite &nbsp;
+              <a href="https://www.ferropoly.ch/datenschutz-cookies/"
+                 target="_blank">www.ferropoly.ch/datenschutz-cookies</a>
+              zu finden.</p>
+          </div>
+        </ferro-card>
+      </div>
+      <div  class="ml-1 mr-1">
+        <ferro-card title="Nutzungsbedingungen">
+          <div>Die Nutzung dieser Software unterliegt den AGB, welche bei der Anmeldung zu diesem Dienst akzeptiert
+            wurden
+            und auf
+            der Ferropoly Webseite publiziert sind.
+          </div>
+          <p class="info">Die wichtigsten Punkte:</p>
+          <ul>
+            <li>Die Durchführung des Spiels erfolgt für Teilnehmer wie Organisator auf eigene Gefahr. Der Betreiber der
+              Webseite
+              kann u.a. für Datenverlust und Serverausfälle nicht haftbar gemacht werden.
+            </li>
+            <li>Eine gewerbliche Benutzung der Software (Organisation von Spielen gegen Vergütung über den effektiven
+              Spesen) ist
+              ohne ausdrückliche Genehmigung des Autors explizit untersagt.
+            </li>
+            <li>Benutzer ohne gültige Email-Adresse (z.B. bei Verwendung von Wegwerf-Adressen) können jederzeit gelöscht
+              werden.
+            </li>
+          </ul>
+        </ferro-card>
 
-    div(class="lg:col-4 md:col-6 sm:col-12")
-      FerroCard(title="Datenschutz")
-        div Es werden nur für das Spiel notwendige Daten erfasst. Mit dem Login über Google und Microsoft sind die öffentlich zugänglichen Daten:
-        ul
-          li Name
-          li Email-Adresse
-          li Avatar Bild
-        p Während dem Spiel werden zudem die Positionsdaten der Spieler aufgenommen. Sämtliche während dem Spiel erfassten Daten werden 30 Tage nach dem Spiel automatisch gelöscht.
-        p Die vollständigen Datenschutz-Infos sind auf der Seite &nbsp;
-          a(href="https://www.ferropoly.ch/datenschutz-cookies/" target="_blank") www.ferropoly.ch/datenschutz-cookies
-          | &nbsp;zu finden.
-    div(class="lg:col-4 md:col-6 sm:col-12")
-      FerroCard(title="Nutzungsbedingungen")
-        div
-          | Die Nutzung dieser Software unterliegt den AGB, welche bei der Anmeldung zu diesem Dienst akzeptiert wurden und auf der Ferropoly Webseite publiziert sind.
-        p.info Die wichtigsten Punkte:
-        ul
-          li Die Durchführung des Spiels erfolgt für Teilnehmer wie Organisator auf eigene Gefahr. Der Betreiber der Webseite kann u.a. für Datenverlust und Serverausfälle nicht haftbar gemacht werden.
-          li Eine gewerbliche Benutzung der Software (Organisation von Spielen gegen Vergütung über den effektiven Spesen) ist ohne ausdrückliche Genehmigung des Autors explizit untersagt.
-          li Benutzer ohne gültige Email-Adresse (z.B. bei Verwendung von Wegwerf-Adressen) können jederzeit gelöscht werden.
-      FerroCard.mt-1(title="Über diese Software")
-        div.info {{title}} V{{version}}
-        div
-          | © 2015-{{currentYear}} Christian Kuster, CH-8342 Wernetshausen,&nbsp;
-          a(:href="emailUrl") {{email}}
-        div Source-Code und Bugtracking auf Github:
-        div
-          a(:href="gitRepo" target="_blank") {{gitRepo}}
-
-    prime-dialog(v-model:visible="ezActive" header="Einzahlungsschein")
-      img.full-width(src="/images/ferropoly_ez.svg")
+        <ferro-card title="Über diese Software" class="mt-2">
+          <div class="info">{{ title }} V{{ version }}</div>
+          <div>© 2015-{{ currentYear }} Christian Kuster, CH-8342 Wernetshausen
+            <a :href="emailUrl">{{ email }}</a>
+          </div>
+          <div>Source-Code und Bugtracking auf Github: <a :href="gitRepo" target="_blank">{{ gitRepo }}</a></div>
+        </ferro-card>
+      </div>
+    </div>
+    <prime-dialog v-model:visible="ezActive" header="Einzahlungsschein">
+      <img class="full-width" src="/images/ferropoly_ez.svg" alt="Ein Einzahlungsschein für das Sponsoring des Ferropoly"/>
+    </prime-dialog>
+  </div>
 
 
 </template>
@@ -59,19 +91,19 @@ import PrimeButton from 'primevue/button';
 import PrimeDialog from 'primevue/dialog';
 
 export default {
-  name      : 'AboutRoot',
+  name:       'AboutRoot',
   components: {MenuBar, FerroCard, PrimeButton, PrimeDialog},
-  filters   : {},
-  mixins    : [],
-  model     : {},
-  props     : {},
-  data      : function () {
+  filters:    {},
+  mixins:     [],
+  model:      {},
+  props:      {},
+  data:       function () {
     return {
       menuBarElements: [],
-      ezActive    : false
+      ezActive:        false
     }
   },
-  computed  : {
+  computed:   {
     currentYear() {
       return DateTime.now().toFormat('yyyy')
     },
@@ -91,9 +123,9 @@ export default {
       return 'mailto:' + packageInfo.author.email;
     }
   },
-  created   : function () {
+  created:    function () {
   },
-  methods   : {}
+  methods:    {}
 }
 
 </script>
