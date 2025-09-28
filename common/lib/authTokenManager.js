3372d1a123afeb099609db682a4694774375fa0b
Christian Kuster <christian@kusti.ch>
2025-09-28 14:31:21 +0200

    refactor: migrate `authTokenManager` to async/await, add `getTokenAsync`
    - Replace callback-based logic with async/await for better readability
    - Add `getTokenAsync` function for streamlined token retrieval
    - Refactor `getNewToken` to utilize `getNewTokenAsync` for cleaner handling

--- common/lib/authTokenManager.js
+++ common/lib/authTokenManager.js
@@ -10,14 +10,22 @@ const tokens     = {};
 const logger     = require('../lib/logger').getLogger('authTokenManager');
 
 const tokenSchema = mongoose.Schema({
-  login     : String,
-  id        : String,
-  issueDate : {type: Date, default: Date.now},
+  login:      String,
+  id:         String,
+  issueDate:  {type: Date, default: Date.now},
   expiryDate: Date
 });
 
 const Token = mongoose.model('Token', tokenSchema);
 
+/**
+ * Retrieves a token associated with the specified user.
+ *
+ * @param {string} user The login or identifier of the user for whom the token is being retrieved.
+ * @param {function} callback The callback function to be executed once the token is retrieved or an error occurs.
+ *                            It receives two arguments: an error object or null, and the retrieved token or null.
+ * @return {Promise<void>} A promise that resolves when the token retrieval process is complete and the callback is invoked.
+ */
 async function getToken(user, callback) {
   let token, err;
   try {
@@ -35,6 +43,45 @@ async function getToken(user, callback) {
   }
 }
 
+/**
+ * Retrieves an asynchronous token for a given user.
+ *
+ * @param {string} user - The login identifier of the user for token retrieval.
+ * @return {Promise<Object>} A promise that resolves to the token object associated with the user, or null if no token is found.
+ */
+async function getTokenAsync(user) {
+  return await Token
+    .findOne()
+    .where('login').equals(user)
+    .exec();
+}
+
+/**
+ * Generates and retrieves a new authentication token for a specified user.
+ *
+ * @param {Object} options - The options for generating the token.
+ * @param {string} options.user - The username for which the token is requested.
+ * @param {string} [options.proposedToken] - The proposed token value, if available.
+ * @return {Promise<string>} A promise that resolves to the ID of the newly generated or existing token.
+ */
+async function getNewTokenAsync(options) {
+  logger.silly(`New authtoken requested for ${options.user} suggesting '${options.proposedToken}'`)
+  let token = await getTokenAsync(options.user);
+  if (token && token.id === options.proposedToken) {
+    // when the authtoken is available and the same, we quit now
+    logger.silly(`${options.user} has already authtoken '${options.proposedToken}'`)
+    return options.proposedToken;
+  }
+  if (!token) {
+    // Create a new token if not existing
+    token = new Token();
+  }
+  token.id    = options.proposedToken || uuid();
+  token.login = options.user;
+  let res = await token.save();
+  logger.info(`User ${options.user} has now authtoken ${token.id}`);
+  return res.id;
+}
 
 module.exports = {
   /**
@@ -43,36 +90,13 @@ module.exports = {
    * @param callback
    */
   getNewToken: function (options, callback) {
-    logger.silly(`New authtoken requested for ${options.user} suggesting '${options.proposedToken}'`)
-    getToken(options.user, async function (err, token) {
-      if (err) {
+    getNewTokenAsync(options)
+      .then(token => {
+        return callback(null, token);
+      })
+      .catch(err => {
         return callback(err);
-      }
-      if (token && token.id === options.proposedToken) {
-        // when the authtoken is available and the same, we quit now
-        logger.silly(`${options.user} has already authtoken '${options.proposedToken}'`)
-        return callback(null, options.proposedToken);
-      }
-      if (!token) {
-        // Create new token if not existing
-        token = new Token();
-      }
-      token.id    = options.proposedToken || uuid();
-      token.login = options.user;
-      let res, errInfo;
-      try {
-        res = await token.save();
-        logger.info(`User ${options.user} has now authtoken ${token.id}`);
-      }
-      catch (ex) {
-        logger.error(ex);
-        errInfo = ex;
-      }
-      finally {
-        callback(errInfo, res.id);
-      }
-    }).then(() => {
-    });
+      })
   },
 
   /**
