e7abb069d9a3607fba5609d754c0e12847b64d79
Christian Kuster <christian@kusti.ch>
2025-10-19 09:02:37 +0200

    refactor: standardize "surname" field in demoUsers
    - Rename "surename" to "surname" across `demoUsers` definitions
    - Update relevant functions and properties to reflect the corrected field name

--- editor/lib/demoUsers.js
+++ editor/lib/demoUsers.js
@@ -5,41 +5,40 @@
 
 const users                  = require('../../common/models/userModel');
 const gravatar               = require('../../common/lib/gravatar');
-const {getUserByMailAddress} = require('../../common/models/userModel');
 const logger                 = require('../../common/lib/logger').getLogger('demoUsers');
 
 const demoUsers = [
-  {forename: 'Heinz', surename: 'Muster', email: 'demo@ferropoly.ch'},
-  {forename: 'Nora', surename: 'Reichle', email: 'team1@ferropoly.ch'},
-  {forename: 'Marius', surename: 'Garatti', email: 'team2@ferropoly.ch'},
-  {forename: 'Sylvia', surename: 'Balmer', email: 'team3@ferropoly.ch'},
-  {forename: 'Jonas', surename: 'Tobler', email: 'team4@ferropoly.ch'},
-  {forename: 'Christine', surename: 'Huber', email: 'team5@ferropoly.ch'},
-  {forename: 'Martin', surename: 'Schiess', email: 'team6@ferropoly.ch'},
-  {forename: 'Lea', surename: 'Kuster', email: 'team7@ferropoly.ch'},
-  {forename: 'Thomas', surename: 'Kunz', email: 'team8@ferropoly.ch'},
-  {forename: 'Bianca', surename: 'Meier', email: 'team9@ferropoly.ch'},
-  {forename: 'Linus', surename: 'Wolfensberger', email: 'team10@ferropoly.ch'},
-  {forename: 'Olivia', surename: 'Osterwalder', email: 'team11@ferropoly.ch'},
-  {forename: 'Simon', surename: 'Rotach', email: 'team12@ferropoly.ch'},
-  {forename: 'Brigitte', surename: 'Buttex', email: 'team13@ferropoly.ch'},
-  {forename: 'Daniel', surename: 'Häberling', email: 'team14@ferropoly.ch'},
-  {forename: 'Franziska', surename: 'Ochsner', email: 'team15@ferropoly.ch'},
-  {forename: 'Bernhard', surename: 'Spiegelberg', email: 'team16@ferropoly.ch'},
-  {forename: 'Claudia', surename: 'Ruckli', email: 'team17@ferropoly.ch'},
-  {forename: 'Richard', surename: 'Droost', email: 'team18@ferropoly.ch'},
-  {forename: 'Nicole', surename: 'Signer', email: 'team19@ferropoly.ch'},
-  {forename: 'Stefan', surename: 'Heinzmann', email: 'team20@ferropoly.ch'},
-  {forename: 'Esther', surename: 'Isler', email: 'team21@ferropoly.ch'},
-  {forename: 'Adriano', surename: 'Baraldo', email: 'team22@ferropoly.ch'},
-  {forename: 'Annina', surename: 'Michels', email: 'team23@ferropoly.ch'},
-  {forename: 'Peter', surename: 'Joss', email: 'team24@ferropoly.ch'},
-  {forename: 'Jeanette', surename: 'Meyer', email: 'team25@ferropoly.ch'},
-  {forename: 'Silvan', surename: 'Rümeli', email: 'team26@ferropoly.ch'},
-  {forename: 'Yvonne', surename: 'Eigenmann', email: 'team27@ferropoly.ch'},
-  {forename: 'Urs', surename: 'Alessi', email: 'team28@ferropoly.ch'},
-  {forename: 'Denise', surename: 'Dütschler', email: 'team29@ferropoly.ch'},
-  {forename: 'Cyrill', surename: 'Looser', email: 'team30@ferropoly.ch'}
+  {forename: 'Heinz', surname: 'Muster', email: 'demo@ferropoly.ch'},
+  {forename: 'Nora', surname: 'Reichle', email: 'team1@ferropoly.ch'},
+  {forename: 'Marius', surname: 'Garatti', email: 'team2@ferropoly.ch'},
+  {forename: 'Sylvia', surname: 'Balmer', email: 'team3@ferropoly.ch'},
+  {forename: 'Jonas', surname: 'Tobler', email: 'team4@ferropoly.ch'},
+  {forename: 'Christine', surname: 'Huber', email: 'team5@ferropoly.ch'},
+  {forename: 'Martin', surname: 'Schiess', email: 'team6@ferropoly.ch'},
+  {forename: 'Lea', surname: 'Kuster', email: 'team7@ferropoly.ch'},
+  {forename: 'Thomas', surname: 'Kunz', email: 'team8@ferropoly.ch'},
+  {forename: 'Bianca', surname: 'Meier', email: 'team9@ferropoly.ch'},
+  {forename: 'Linus', surname: 'Wolfensberger', email: 'team10@ferropoly.ch'},
+  {forename: 'Olivia', surname: 'Osterwalder', email: 'team11@ferropoly.ch'},
+  {forename: 'Simon', surname: 'Rotach', email: 'team12@ferropoly.ch'},
+  {forename: 'Brigitte', surname: 'Buttex', email: 'team13@ferropoly.ch'},
+  {forename: 'Daniel', surname: 'Häberling', email: 'team14@ferropoly.ch'},
+  {forename: 'Franziska', surname: 'Ochsner', email: 'team15@ferropoly.ch'},
+  {forename: 'Bernhard', surname: 'Spiegelberg', email: 'team16@ferropoly.ch'},
+  {forename: 'Claudia', surname: 'Ruckli', email: 'team17@ferropoly.ch'},
+  {forename: 'Richard', surname: 'Droost', email: 'team18@ferropoly.ch'},
+  {forename: 'Nicole', surname: 'Signer', email: 'team19@ferropoly.ch'},
+  {forename: 'Stefan', surname: 'Heinzmann', email: 'team20@ferropoly.ch'},
+  {forename: 'Esther', surname: 'Isler', email: 'team21@ferropoly.ch'},
+  {forename: 'Adriano', surname: 'Baraldo', email: 'team22@ferropoly.ch'},
+  {forename: 'Annina', surname: 'Michels', email: 'team23@ferropoly.ch'},
+  {forename: 'Peter', surname: 'Joss', email: 'team24@ferropoly.ch'},
+  {forename: 'Jeanette', surname: 'Meyer', email: 'team25@ferropoly.ch'},
+  {forename: 'Silvan', surname: 'Rümeli', email: 'team26@ferropoly.ch'},
+  {forename: 'Yvonne', surname: 'Eigenmann', email: 'team27@ferropoly.ch'},
+  {forename: 'Urs', surname: 'Alessi', email: 'team28@ferropoly.ch'},
+  {forename: 'Denise', surname: 'Dütschler', email: 'team29@ferropoly.ch'},
+  {forename: 'Cyrill', surname: 'Looser', email: 'team30@ferropoly.ch'}
 ];
 
 module.exports = {
@@ -50,7 +49,7 @@ module.exports = {
    * @returns {string}
    */
   getTeamLeaderName: function (index) {
-    return demoUsers[1 + index].forename + ' ' + demoUsers[1 + index].surename;
+    return demoUsers[1 + index].forename + ' ' + demoUsers[1 + index].surname;
   },
 
   /**
@@ -77,7 +76,7 @@ module.exports = {
         let user = new users.Model();
 
         user.personalData.forename = u.forename;
-        user.personalData.surename = u.surename;
+        user.personalData.surname = u.surname;
         user.personalData.email = u.email;
         user.personalData.avatar =gravatar.getUrl(u.email);
          user.login.verifiedEmail = true;
